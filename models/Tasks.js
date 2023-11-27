bdafc2263f36a961783ac2a9d5ba83d90ccab260
M Yang <miloyang9@gmail.com>
2023-11-27 21:19:53 +1100

    updated index.js for api routes

--- models/Tasks.js
+++ models/Tasks.js
@@ -4,18 +4,7 @@ const sequelize = require('../config/connection');
 class Tasks extends Model {}
 
 Tasks.init(
-    {
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-    },
+
 {
     sequelize,
     timestamps: false,
