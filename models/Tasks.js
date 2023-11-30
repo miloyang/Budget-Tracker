8f24e47e7f5a00da55b2ed3aaebe6f94ab4039b8
Muhammad Khabbaz <muhammad.khabbaz@outlook.com>
2023-11-30 18:50:36 +1100

    js and html for kanban complete

--- models/Tasks.js
+++ models/Tasks.js
@@ -4,14 +4,24 @@ const sequelize = require('../config/connection');
 class Tasks extends Model {}
 
 Tasks.init(
-
-{
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'task',
-}
+  }
 );
 
 module.exports = Tasks;
