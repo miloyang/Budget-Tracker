5a614e0c5464930ae78c05cdc73ede17e841d9ac
Muhammad Khabbaz <muhammad.khabbaz@outlook.com>
2023-12-04 17:01:00 +1100

    edited project.handlebars however needs to be linked to database and tasks button not coded yet, multer completed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const taskRoutes = require('./taskRoutes');
+const uploadRoutes = require('./uploadRoutes')
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/tasks', taskRoutes);
+router.use('/upload', uploadRoutes);
 
 module.exports = router; 
--- controllers/api/uploadRoutes.js
+++ controllers/api/uploadRoutes.js
@@ -1,37 +1,42 @@
-const path = require("path")
 const multer = require("multer")
+const router = require('express').Router();
 const fs = require("fs");
+const path = require("path")
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null,"images")
+        cb(null,"public/images")
     },
     filename: (req, file, cb) => {
         console.log(file)
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const fileExtension = path.extname(file.originalname); // Extracts file extension
-        const imgName = req.body.imgname
         cb(null, "image" + fileExtension);
     }
 })
 
 const upload = multer ({storage: storage})
 
-app.get('/upload', (req,res) => {
+router.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post("/upload", upload.single("image"), (req, res) => {
+router.post("/", upload.single("image"), (req, res) => {
     const tempPath = req.file.path;
     const imgName = req.body.imgname || 'default';
     const newFileName = imgName + path.extname(req.file.originalname);
-    const newPath = path.join('images', newFileName);
+    const newPath = path.join('public/images', newFileName);
+
+    // Relative path for the response
+    const imagePath = `/images/${newFileName}`;
 
     fs.rename(tempPath, newPath, (err) => {
         if (err) {
             console.error(err);
             return res.status(500).send("Error renaming file");
         }
-        res.send(newFileName + "uploaded");
+        // Send the response after the file is successfully renamed
+        res.json({ imagePath: `/images/${newFileName}` });
     });
 });
+
+module.exports = router
